f7e0c1c8f56a4b0e4be060f7dbefac1cd181de43
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-24 16:03:26 +0300

    fix

---
+++ scripts/postInstall.sh
@@ -0,0 +1,11 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#wait until the server is ready
+echo "Waiting for software to be ready ..."
+sleep 90s;
+
+docker-compose down;
+docker-compose up -d;
+
+sleep 120s;
